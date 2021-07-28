a9b022338d61cd7b51db496bb2efe1737cd71830
normas <burgambo@gmail.com>
2021-07-29 00:12:52 +0300

    Create js_d3_chart_tests.js

---
+++ js_d3_chart_tests.js
@@ -0,0 +1,13 @@
+	
+	import * as d3 from "https://cdn.skypack.dev/d3@7";
+
+	const div = d3.selectAll("div");
+
+	console.log('loaded...');
+
+	values = Array(5) [1, 3, 2, 6, 4];
+	height = 200
+
+	import {chart as chart1} with {values as data, height} from "@d3/histogram"
+	chart1;
+
