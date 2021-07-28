6ac4b9e6a80eae170b8b74dbe526a30f21a89e20
normas <burgambo@gmail.com>
2021-07-29 00:29:47 +0300

    Update js_d3_chart_tests.js

--- js_d3_chart_tests.js
+++ js_d3_chart_tests.js
@@ -1,9 +1,11 @@
 	
-	import * as d3 from "https://cdn.skypack.dev/d3@7";
+import * as d3 from "https://cdn.skypack.dev/d3@7";
+
+
+export function test_chart1(ctx, length, x, y, color) {
+	console.log('click!');
 
 	const div = d3.selectAll("div");
-
-	console.log('loaded...');
 
 	values = Array(5) [1, 3, 2, 6, 4];
 	height = 200
@@ -11,3 +13,4 @@
 	import {chart as chart1} with {values as data, height} from "@d3/histogram"
 	chart1;
 
+}
