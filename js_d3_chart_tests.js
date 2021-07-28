a700eb6a0553f9f82ee81b62aaf0d8c24bf27065
normas <burgambo@gmail.com>
2021-07-29 01:53:40 +0300

    Update js_d3_chart_tests.js

--- js_d3_chart_tests.js
+++ js_d3_chart_tests.js
@@ -1,15 +1,38 @@
-	
-import * as d3 from "https://cdn.skypack.dev/d3@7";
 
-console.log('test loaded');
 
-async function test_chart1() {
-	console.log('click!');
 
-	values = Array(5) [1, 3, 2, 6, 4];
-	height = 200
-
-	import {chart as chart1} with {values as data, height} from "@d3/histogram"
-	chart1;
-
-}
+var ctx = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [9, 11, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
